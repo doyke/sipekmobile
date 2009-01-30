13d6f19e90106f4eb6d8ff6a09a56517d0123567
sasacoh <sasacoh@1ae78998-ed05-11dd-9d8c-a70aa0a93a20>
2009-01-30 08:08:44 +0000

    Update version info

--- About.cs
+++ About.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace SipekMobile
 {
@@ -19,5 +20,6 @@ namespace SipekMobile
     {
       Close();
     }
+
   }
 }
